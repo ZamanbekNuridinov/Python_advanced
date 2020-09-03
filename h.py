5bbf775b89a7f2521491e02f133d02b0e25c4bcb
ZamanbekNuridinov <nuridinov.zamanbek.911@gmail.com>
2020-09-03 18:11:21 +0600

    y=kx+b

--- h.py
+++ h.py
@@ -1,1 +1,19 @@
-print("Helo world")
+a=int(input())
+a1=int(input())
+
+b=int(input())
+b1=int(input())
+
+c=int(input())
+c1=int(input())
+
+k = int((a-b)/(a1-b1))
+b=a1-k*a
+if(a1==b1):
+    k==0
+if(c==k*c1+b):
+    print("point in the line")
+if(c > k*c1+b):
+    print("up")
+else:
+    print("down")
