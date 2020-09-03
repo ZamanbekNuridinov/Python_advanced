c9dcb45143dd4ac3da0af1cae23700229a04aa0b
ZamanbekNuridinov <nuridinov.zamanbek.911@gmail.com>
2020-09-03 22:12:55 +0600

    Check login and password

--- 2.py
+++ 2.py
@@ -1,1 +1,12 @@
-print("H")
+login=["admin","admin1"]
+password=["pass", "pass1"]
+
+a=input()
+b=input()
+
+for i in range(len(login)):
+    if(a==login[i] and b==password[i]):
+        print("Welcome!")
+        break
+else:
+    print("Not correct!")
